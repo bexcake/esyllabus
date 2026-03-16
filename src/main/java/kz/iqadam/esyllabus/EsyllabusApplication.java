1ccfadfc72356f5438a6d0613d8715eab02540fc
bexcake <jugdge678@gmail.com>
2026-03-16 11:03:39 +0500

    first commit

---
+++ src/main/java/kz/iqadam/esyllabus/EsyllabusApplication.java
@@ -0,0 +1,13 @@
+package kz.iqadam.esyllabus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsyllabusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsyllabusApplication.class, args);
+    }
+
+}
